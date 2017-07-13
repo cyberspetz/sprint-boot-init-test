79cc2cc6bf6fc8c391e72234a55e47e0d696cfc1
yaroslav.kisylychka <yaroslav.kisylychka@spika.com>
2017-07-13 17:27:46 +0300

    Initial project files

---
+++ src/main/java/com/nix/GitflowApplication.java
@@ -0,0 +1,12 @@
+package com.nix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowApplication.class, args);
+    }
+}
